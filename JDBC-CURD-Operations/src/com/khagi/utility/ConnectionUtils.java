795cb5225f598356f26031776ee16e1771a6f5fa
Chandu <Chandu@DESKTOP-QGP1KHO>
2021-01-03 20:53:49 +0530

    Connection Established message has been added

--- JDBC-CURD-Operations/src/com/khagi/utility/ConnectionUtils.java
+++ JDBC-CURD-Operations/src/com/khagi/utility/ConnectionUtils.java
@@ -6,13 +6,18 @@ import java.sql.SQLException;
 
 public class ConnectionUtils {
 	
-	Connection connection = null;
+	
+	public static void main(String[] args) {
+		getConnection();
+	}
 
-	public Connection getConnection() {
+	public static Connection getConnection() {
+		Connection connection = null;
 		
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "Chandu");			
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "Chandu");
+			System.out.println("Connection Established Successfully....");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
